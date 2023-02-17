8d6b972ebf5c552956f383975143bd2731d06138
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-02-17 21:02:09 +0700

    create edit category function and try to implement methodOverride

--- app/category/controller.js
+++ app/category/controller.js
@@ -28,4 +28,37 @@ module.exports = {
       res.status(500).json({ error: error.message });
     }
   },
+
+  viewEdit: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const category = await Category.findById(id);
+
+      if (!category)
+        return res.status(404).json({ error: "Category not found" });
+
+      res.render("admin/category/edit", { title: "Edit Category", category });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  edit: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { name } = req.body;
+      const timestamp = Date.now();
+      const categoryUpdated = await Category.findByIdAndUpdate(id, {
+        name,
+        timestamp,
+      });
+
+      if (!categoryUpdated)
+        return res.status(404).json({ error: "Category not found" });
+
+      res.redirect("/category");
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
 };
--- app/category/router.js
+++ app/category/router.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { index, viewCreate, create } = require("./controller");
+const { index, viewCreate, create, viewEdit, edit } = require("./controller");
 const router = express.Router();
 
 router.get("/", index);
 router.get("/create", viewCreate);
 router.post("/create", create);
+router.get("/edit/:id", viewEdit);
+router.put("/edit/:id", edit);
 
 module.exports = router;
