0e308e61a180650de912fd6abafe2c7b5e3b3db0
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-03-08 11:53:32 +0700

    comment session auth, idk why dont work in vercel

--- app/payment/router.js
+++ app/payment/router.js
@@ -11,7 +11,7 @@ const {
 const router = express.Router();
 const { isLogin } = require("../../middleware/auth");
 
-router.use(isLogin);
+// router.use(isLogin);
 router.get("/", index);
 router.get("/create", viewCreate);
 router.post("/create", create);
